688b6d861d080909c7d9d3b165a4639d2d4fb505
Yaroslav Klymko <t3hnar@gmail.com>
2018-07-10 16:55:38 +0200

    Re #36 Format ActionBatch

--- journal/src/main/scala/com/evolutiongaming/kafka/journal/ActionBatch.scala
+++ journal/src/main/scala/com/evolutiongaming/kafka/journal/ActionBatch.scala
@@ -9,16 +9,19 @@ object ActionBatch {
 
   def apply(actions: Iterable[Action]): ActionBatch = {
 
+    def onDelete(b: NonEmpty, a: Delete) = {
+      val deleteTo = b.deleteTo.fold(a.seqNr) { _ max a.seqNr } min b.lastSeqNr
+      NonEmpty(b.lastSeqNr, Some(deleteTo))
+    }
+
     actions.foldLeft[ActionBatch](Empty) {
       case (b: NonEmpty, a: Append) => NonEmpty(a.range.to, b.deleteTo)
-      case (b: NonEmpty, a: Delete) =>
-        val deleteTo = b.deleteTo.fold(a.seqNr) { _ max a.seqNr } min b.lastSeqNr
-        NonEmpty(b.lastSeqNr, Some(deleteTo))
+      case (b: NonEmpty, a: Delete) => onDelete(b, a)
       case (b: DeleteTo, a: Append) => NonEmpty(a.range.to, Some(b.seqNr min a.range.from.prev))
       case (b: DeleteTo, a: Delete) => DeleteTo(b.seqNr max a.seqNr)
-      case (Empty, a: Append)       => NonEmpty(a.range.to, None)
-      case (Empty, a: Delete)       => DeleteTo(a.seqNr)
-      case (b, _: Mark)             => b
+      case (Empty      , a: Append) => NonEmpty(a.range.to, None)
+      case (Empty      , a: Delete) => DeleteTo(a.seqNr)
+      case (b          , _: Mark)   => b
     }
   }
 
