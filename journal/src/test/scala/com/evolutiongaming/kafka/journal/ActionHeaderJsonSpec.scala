5a13f3437bc15a799b90c5e651ab912581cd64f7
Yaroslav Klymko <t3hnar@gmail.com>
2018-09-24 18:22:42 +0200

    fix ActionHeaderJsonSpec

--- journal/src/test/scala/com/evolutiongaming/kafka/journal/ActionHeaderJsonSpec.scala
+++ journal/src/test/scala/com/evolutiongaming/kafka/journal/ActionHeaderJsonSpec.scala
@@ -9,25 +9,26 @@ class ActionHeaderJsonSpec extends FunSuite with Matchers {
   for {
     origin <- List(Some(Origin("origin")), None)
   } {
+    val originStr = origin.fold("None")(_.toString)
     for {
       payloadType <- List(PayloadType.Binary, PayloadType.Json)
     } {
       test(s"Append format, origin: $origin, payloadType: $payloadType") {
         val range = SeqRange(1, 5)
         val header = ActionHeader.Append(range, origin, payloadType)
-        verify(header, s"Append.$origin.$payloadType")
+        verify(header, s"Append.$originStr.$payloadType")
       }
     }
 
     test(s"Delete format, origin: $origin") {
       val seqNr = 3.toSeqNr
       val header = ActionHeader.Delete(seqNr, origin)
-      verify(header, s"Delete.$origin")
+      verify(header, s"Delete.$originStr")
     }
 
     test(s"Mark format, origin: $origin, ") {
       val header = ActionHeader.Mark("id", origin)
-      verify(header, s"Mark.$origin")
+      verify(header, s"Mark.$originStr")
     }
   }
 
