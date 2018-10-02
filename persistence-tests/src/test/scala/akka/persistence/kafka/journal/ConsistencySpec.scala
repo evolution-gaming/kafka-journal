1f88d7ef7541c5e4ad0aa60bc7f05ab41fc53de1
Yaroslav Klymko <t3hnar@gmail.com>
2018-10-02 19:14:07 +0200

    decrease number of events in ConsistencySpec

--- persistence-tests/src/test/scala/akka/persistence/kafka/journal/ConsistencySpec.scala
+++ persistence-tests/src/test/scala/akka/persistence/kafka/journal/ConsistencySpec.scala
@@ -74,7 +74,7 @@ class ConsistencySpec extends PluginSpec(ConfigFactory.load("consistency.conf"))
       recoverEvents("new_id") shouldEqual Nil
     }
 
-    val numberOfEvents = 100000
+    val numberOfEvents = 10000
 
     s"recover $numberOfEvents events" in {
       val ref = PersistenceRef()
