7787b6491b9a27a0aa73dd3e140ef798bc16d57c
Yaroslav Klymko <yklymko@evolutiongaming.com>
2019-03-24 00:09:18 +0100

    improve PersistentBinary

--- persistence/src/main/scala/akka/persistence/kafka/journal/PersistentBinary.scala
+++ persistence/src/main/scala/akka/persistence/kafka/journal/PersistentBinary.scala
@@ -1,12 +1,10 @@
 package akka.persistence.kafka.journal
 
-import java.lang.{Byte => ByteJ}
-
 import akka.persistence.PersistentRepr
 import com.evolutiongaming.kafka.journal.{Bytes, FromBytes, ToBytes}
 import com.evolutiongaming.serialization.SerializedMsg
 import scodec.bits.BitVector
-import scodec.{Attempt, Codec, DecodeResult, Err, SizeBound, codecs}
+import scodec.codecs._
 
 final case class PersistentBinary(
   manifest: String,
@@ -16,37 +14,9 @@ final case class PersistentBinary(
 
 object PersistentBinary {
 
-  private def codecCustom[A](codec: Codec[A], sizeCodec: Codec[Int] = codecs.int32): Codec[A] = new Codec[A] {
-
-    def decode(bits: BitVector) = {
-      for {
-        result <- sizeCodec.decode(bits)
-        size    = result.value
-        _      <- Attempt.guard(size >= 0, Err(s"requires positive size, got $size"))
-        bits    = result.remainder
-        (bits1, remainder) = bits.splitAt(size * ByteJ.SIZE.toLong)
-        result <- codec.decode(bits1)
-      } yield {
-        DecodeResult(result.value, remainder)
-      }
-    }
-
-    def encode(value: A) = {
-      for {
-        bits     <- codec.encode(value)
-        size      = bits.size.toInt / ByteJ.SIZE
-        sizeBits <- sizeCodec.encode(size)
-      } yield {
-        sizeBits ++ bits
-      }
-    }
-
-    val sizeBound = SizeBound.atLeast(ByteJ.SIZE.toLong)
-  }
-
   private val codec = {
-    val codecSerializedMsg = (codecs.int32 :: codecs.utf8_32 :: codecCustom(codecs.bytes)).as[SerializedMsg]
-    (codecs.utf8_32 :: codecs.utf8_32 :: codecSerializedMsg).as[PersistentBinary]
+    val codecSerializedMsg = (int32 :: utf8_32 :: variableSizeBytes(int32, bytes)).as[SerializedMsg]
+    (utf8_32 :: utf8_32 :: codecSerializedMsg).as[PersistentBinary]
   }
 
 
