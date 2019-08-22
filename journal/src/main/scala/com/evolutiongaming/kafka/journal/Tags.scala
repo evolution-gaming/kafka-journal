2ecc8c0a49d96517cc9f86912982589bb368003e
Yaroslav Klymko <t3hnar@gmail.com>
2019-08-22 17:31:57 +0300

    cleanup

--- journal/src/main/scala/com/evolutiongaming/kafka/journal/Tags.scala
+++ journal/src/main/scala/com/evolutiongaming/kafka/journal/Tags.scala
@@ -1,51 +1,10 @@
 package com.evolutiongaming.kafka.journal
 
-import java.lang.{Integer => IntJ}
-import java.nio.ByteBuffer
-
-import com.evolutiongaming.kafka.journal.FromBytes.Implicits._
-import com.evolutiongaming.kafka.journal.ToBytes.Implicits._
-import com.evolutiongaming.kafka.journal.util.ByteBufferHelper._
 import scodec.{Codec, codecs}
-
-import scala.annotation.tailrec
 
 object Tags {
 
   val Empty: Tags = Set.empty
-
-  implicit val TagsToBytes: ToBytes[Tags] = new ToBytes[Tags] {
-    def apply(tags: Tags) = {
-      if (tags.isEmpty) Bytes.Empty
-      else {
-        val bytes = tags.toList.map(_.toBytes)
-        val length = bytes.foldLeft(0) { (length, bytes) => length + IntJ.BYTES + bytes.length }
-        val buffer = ByteBuffer.allocate(length)
-        bytes.foreach(buffer.writeBytes)
-        buffer.array()
-      }
-    }
-  }
-
-  implicit val TagsFromBytes: FromBytes[Tags] = new FromBytes[Tags] {
-    def apply(bytes: Bytes) = {
-      if (bytes.isEmpty) Set.empty
-      else {
-        val buffer = ByteBuffer.wrap(bytes)
-
-        @tailrec
-        def loop(tags: Tags): Tags = {
-          if (!buffer.hasRemaining) tags
-          else {
-            val tag = buffer.readBytes.fromBytes[Tag]
-            loop(tags + tag)
-          }
-        }
-
-        loop(Set.empty)
-      }
-    }
-  }
 
   implicit val CodecTags: Codec[Tags] = {
     val codec = codecs.list(codecs.utf8_32).xmap[Tags](_.toSet, _.toList)
--- journal/src/test/scala/com/evolutiongaming/kafka/journal/PayloadBinaryFromStr.scala
+++ journal/src/test/scala/com/evolutiongaming/kafka/journal/PayloadBinaryFromStr.scala
@@ -1,6 +1,7 @@
 package com.evolutiongaming.kafka.journal
 
 object PayloadBinaryFromStr {
+
   def apply(a: String): Payload.Binary = {
     val attempt = scodec.codecs.utf8.encode(a)
     val byteVector = attempt.require.toByteVector
