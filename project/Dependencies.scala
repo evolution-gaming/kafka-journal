3398741bc494a29635dde865d9e5fc6112751523
Yaroslav Klymko <t3hnar@gmail.com>
2019-09-12 19:56:14 +0200

    bump retry

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -20,7 +20,7 @@ object Dependencies {
   val `cassandra-sync`     = "com.evolutiongaming"    %% "cassandra-sync"        % "1.0.3"
   val `cats-helper`        = "com.evolutiongaming"    %% "cats-helper"           % "1.0.0"
   val random               = "com.evolutiongaming"    %% "random"                % "0.0.5"
-  val retry                = "com.evolutiongaming"    %% "retry"                 % "0.0.5"
+  val retry                = "com.evolutiongaming"    %% "retry"                 % "0.0.6"
   val sstream              = "com.evolutiongaming"    %% "sstream"               % "0.0.3"
   val skafka               = "com.evolutiongaming"    %% "skafka"                % "6.1.4"
   val scache               = "com.evolutiongaming"    %% "scache"                % "0.0.11"
