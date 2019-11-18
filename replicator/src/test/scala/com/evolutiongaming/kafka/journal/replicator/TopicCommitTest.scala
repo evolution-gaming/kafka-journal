5c15cc89e3b8d2c875e5030a57f6f200c2768d7f
Yaroslav Klymko <t3hnar@gmail.com>
2019-11-18 22:08:29 +0100

    stabilise TopicCommitTest

--- replicator/src/test/scala/com/evolutiongaming/kafka/journal/replicator/TopicCommitTest.scala
+++ replicator/src/test/scala/com/evolutiongaming/kafka/journal/replicator/TopicCommitTest.scala
@@ -2,7 +2,7 @@ package com.evolutiongaming.kafka.journal.replicator
 
 import cats.data.{NonEmptyMap => Nem}
 import cats.implicits._
-import cats.effect.{IO, Timer}
+import cats.effect.{Clock, IO, Timer}
 import com.evolutiongaming.kafka.journal.IOSuite._
 import cats.effect.concurrent.{Deferred, Ref}
 import com.evolutiongaming.skafka.{Offset, Partition}
@@ -13,23 +13,42 @@ import scala.concurrent.duration._
 class TopicCommitTest extends AsyncFunSuite with Matchers{
 
   test("delayed") {
-    val result = for {
-      commitsRef <- Ref[IO].of(List.empty[Nem[Partition, Offset]])
-      deferred   <- Deferred[IO, Unit]
-      commit = new TopicCommit[IO] {
+
+    def commitOf(
+      deferred: Deferred[IO, Unit],
+      commitsRef: Ref[IO, List[Nem[Partition, Offset]]])(implicit
+      clock: Clock[IO]
+    ) = {
+      val commit = new TopicCommit[IO] {
         def apply(offsets: Nem[Partition, Offset]) = {
           commitsRef.update { offsets :: _ } *> deferred.complete(())
         }
       }
-      commit  <- TopicCommit.delayed(200.millis, commit)
-      _       <- commit(Nem.of((0, 0L)))
-      offsets <- commitsRef.get
-      _        = offsets shouldEqual List.empty
-      _       <- Timer[IO].sleep(400.millis)
-      _       <- commit(Nem.of((1, 1L)))
-      _       <- deferred.get
-      offsets <- commitsRef.get
-      _        = offsets shouldEqual List(Nem.of((0, 0L), (1, 1L)))
+
+      TopicCommit.delayed(10.millis, commit)
+    }
+
+    def clockOf(ref: Ref[IO, FiniteDuration]): Clock[IO] = {
+      new Clock[IO] {
+        def realTime(unit: TimeUnit): IO[Long] = monotonic(unit)
+        def monotonic(unit: TimeUnit): IO[Long] = ref.get.map { _.toUnit(unit).toLong }
+      }
+    }
+
+    val result = for {
+      commitsRef <- Ref[IO].of(List.empty[Nem[Partition, Offset]])
+      deferred   <- Deferred[IO, Unit]
+      clockRef   <- Ref[IO].of(0.millis)
+      clock       = clockOf(clockRef)
+      commit     <- commitOf(deferred, commitsRef)(clock)
+      _          <- commit(Nem.of((0, 0L)))
+      offsets    <- commitsRef.get
+      _           = offsets shouldEqual List.empty
+      _          <- clockRef.set(20.millis)
+      _          <- commit(Nem.of((1, 1L)))
+      _          <- deferred.get
+      offsets    <- commitsRef.get
+      _           = offsets shouldEqual List(Nem.of((0, 0L), (1, 1L)))
     } yield {}
     result.run()
   }
