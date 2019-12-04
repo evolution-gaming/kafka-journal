eac40727929b36ccc2b793a68369ce695c79504c
Yaroslav Klymko <t3hnar@gmail.com>
2019-12-04 23:15:02 +0100

    downgrade play-json

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -6,7 +6,7 @@ object Dependencies {
   val `scala-java8-compat` = "org.scala-lang.modules" %% "scala-java8-compat"    % "0.9.0"
   val prometheus           = "io.prometheus"           % "simpleclient"          % "0.6.0"
   val `cassandra-driver`   = "com.datastax.cassandra"  % "cassandra-driver-core" % "3.7.2"
-  val `play-json`          = "com.typesafe.play"      %% "play-json"             % "2.8.0"
+  val `play-json`          = "com.typesafe.play"      %% "play-json"             % "2.7.4"
   val `kind-projector`     = "org.typelevel"           % "kind-projector"        % "0.10.3"
   val `executor-tools`     = "com.evolutiongaming"    %% "executor-tools"        % "1.0.2"
   val `config-tools`       = "com.evolutiongaming"    %% "config-tools"          % "1.0.3"
@@ -22,7 +22,7 @@ object Dependencies {
   val random               = "com.evolutiongaming"    %% "random"                % "0.0.6"
   val retry                = "com.evolutiongaming"    %% "retry"                 % "1.0.2"
   val sstream              = "com.evolutiongaming"    %% "sstream"               % "0.2.0"
-  val skafka               = "com.evolutiongaming"    %% "skafka"                % "8.0.1"
+  val skafka               = "com.evolutiongaming"    %% "skafka"                % "8.0.2"
   val scache               = "com.evolutiongaming"    %% "scache"                % "2.1.1"
 
   object Cats {
