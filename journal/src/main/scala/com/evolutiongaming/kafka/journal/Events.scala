9c249d176f7878f84d185f5b0d5843cfd1e3ae85
Yaroslav Klymko <t3hnar@gmail.com>
2019-12-23 13:11:17 +0100

    improve Events.codecEvents

--- journal/src/main/scala/com/evolutiongaming/kafka/journal/Events.scala
+++ journal/src/main/scala/com/evolutiongaming/kafka/journal/Events.scala
@@ -1,21 +1,31 @@
 package com.evolutiongaming.kafka.journal
 
 import cats.data.{NonEmptyList => Nel}
+import cats.implicits._
+import java.lang.{Byte => ByteJ}
 import com.evolutiongaming.kafka.journal.util.ScodecHelper._
 import scodec.bits.ByteVector
 import scodec.{Codec, codecs}
 
-final case class Events(events: Nel[Event])
+final case class Events(events: Nel[Event]/*, metadata: Events.Metadata*/)
 
 object Events {
 
   implicit val codecEvents: Codec[Events] = {
     val eventsCodec = nelCodec(codecs.listOfN(codecs.int32, codecs.variableSizeBytes(codecs.int32, Codec[Event])))
-    val version = ByteVector.fromByte(0)
-    (codecs.constant(version) ~> eventsCodec).as[Events]
+    val default = codecs.ignore(ByteJ.SIZE) ~> eventsCodec
+    val version1 = codecs.constant(ByteVector.fromByte(0)) ~> eventsCodec
+    codecs.choice(version1, default).as[Events]
   }
 
   implicit def eventsToBytes[F[_] : FromAttempt]: ToBytes[F, Events] = ToBytes.fromEncoder
 
   implicit def eventsFromBytes[F[_] : FromAttempt]: FromBytes[F, Events] = FromBytes.fromDecoder
+
+
+  final case class Metadata(expireAfter: Option[ExpireAfter])
+
+  object Metadata {
+    val empty: Metadata = Metadata(none)
+  }
 }
---
+++ journal/src/test/scala/com/evolutiongaming/kafka/journal/EventsTest.scala
@@ -0,0 +1,29 @@
+package com.evolutiongaming.kafka.journal
+
+import cats.data.{NonEmptyList => Nel}
+import cats.implicits._
+import com.evolutiongaming.kafka.journal.util.ScodecHelper.{nelCodec, _}
+import org.scalatest.FunSuite
+import org.scalatest.matchers.should.Matchers
+import scodec.bits.ByteVector
+import scodec.{Attempt, Codec, codecs}
+
+class EventsTest extends FunSuite with Matchers {
+
+  test("decode newer version") {
+    val codec = {
+      val eventsCodec = nelCodec(codecs.listOfN(codecs.int32, codecs.variableSizeBytes(codecs.int32, Codec[Event])))
+      val version = ByteVector.fromByte(100)
+      (codecs.constant(version) ~> eventsCodec).as[Events]
+    }
+
+    val events = Events(Nel.of(Event(SeqNr.min)))
+    val actual = for {
+      bits   <- codec.encode(events)
+      result <- Events.codecEvents.decode(bits)
+    } yield {
+      result.value
+    }
+    actual shouldEqual events.pure[Attempt]
+  }
+}
