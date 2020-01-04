8f9e29d942c25544abaa4cc5e38d118a02c14e73
Yaroslav Klymko <t3hnar@gmail.com>
2020-01-04 22:50:04 +0100

    cleanup KafkaJournalConfig

--- persistence/src/main/scala/akka/persistence/kafka/journal/KafkaJournalConfig.scala
+++ persistence/src/main/scala/akka/persistence/kafka/journal/KafkaJournalConfig.scala
@@ -1,18 +1,12 @@
 package akka.persistence.kafka.journal
 
-import cats.implicits._
-import com.evolutiongaming.config.ConfigHelper._
 import com.evolutiongaming.kafka.journal.Journal.CallTimeThresholds
 import com.evolutiongaming.kafka.journal.JournalConfig
 import com.evolutiongaming.kafka.journal.eventual.cassandra.EventualCassandraConfig
-import com.typesafe.config.Config
-import pureconfig.error.{ConfigReaderFailures, ThrowableFailure}
-import pureconfig.{ConfigCursor, ConfigReader, ConfigSource}
+import pureconfig.ConfigReader
 import pureconfig.generic.semiauto.deriveReader
 
 import scala.concurrent.duration._
-import scala.reflect.ClassTag
-import scala.util.Try
 
 final case class KafkaJournalConfig(
   journal: JournalConfig = JournalConfig.default,
@@ -26,50 +20,5 @@ object KafkaJournalConfig {
 
   val default: KafkaJournalConfig = KafkaJournalConfig()
 
-
-  /*implicit val configReaderKafkaJournalConfig: ConfigReader[KafkaJournalConfig] = {
-    cursor: ConfigCursor => {
-      for {
-        cursor    <- cursor.asObjectCursor
-        cassandra  = Try { fromConfig(cursor.value.toConfig, default) }
-        cassandra <- cassandra.toEither.leftMap(a => ConfigReaderFailures(ThrowableFailure(a, cursor.location)))
-      } yield cassandra
-    }
-
-
-  }*/
-
   implicit val configReaderKafkaJournalConfig: ConfigReader[KafkaJournalConfig] = deriveReader
-
-
-//  def apply(config: Config): KafkaJournalConfig = apply(config, default)
-
-//  def apply(config: Config, default: => KafkaJournalConfig): KafkaJournalConfig = fromConfig(config, default)
-
-  /*private def fromConfig(config: Config, default: => KafkaJournalConfig): KafkaJournalConfig = {
-
-    def get[T: FromConf](name: String) = config.getOpt[T](name)
-
-    val callTimeThresholds = ConfigSource.fromConfig(config)
-      .at("call-time-thresholds")
-      .load[CallTimeThresholds]
-      .getOrElse(CallTimeThresholds.default)
-
-    val source = ConfigSource.fromConfig(config)
-
-    def getOrThrow[A : ConfigReader : ClassTag](name: String) = {
-      val source1 = source.at(name)
-      source1.value().fold(_ => none[A], _ => source1.loadOrThrow[A].some)
-    }
-
-    source
-
-    KafkaJournalConfig(
-      journal = JournalConfig.fromConfig(config, default.journal),
-      cassandra = getOrThrow[EventualCassandraConfig]("cassandra") getOrElse default.cassandra,
-      startTimeout = get[FiniteDuration]("start-timeout") getOrElse default.startTimeout,
-      stopTimeout = get[FiniteDuration]("stop-timeout") getOrElse default.stopTimeout,
-      maxEventsInBatch = get[Int]("max-events-in-batch") getOrElse default.maxEventsInBatch,
-      callTimeThresholds = callTimeThresholds)
-  }*/
 }
