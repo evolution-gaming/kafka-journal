f9b0852996ba2afa94b995ecc196c9b4bee2f228
Yaroslav Klymko <t3hnar@gmail.com>
2020-02-26 10:08:08 +0100

    increase default of KafkaJournalConfig.maxEventsInBatch from 10 to 100

--- persistence/src/main/scala/akka/persistence/kafka/journal/Batching.scala
+++ persistence/src/main/scala/akka/persistence/kafka/journal/Batching.scala
@@ -13,18 +13,14 @@ object Batching {
   def apply[F[_]](implicit F: Batching[F]): Batching[F] = F
 
 
-  def disabled[F[_] : Applicative]: Batching[F] = new Batching[F] {
-    def apply(aws: List[AtomicWrite]) = aws.map(List(_)).pure[F]
-  }
+  def disabled[F[_] : Applicative]: Batching[F] = (aws: List[AtomicWrite]) => aws.map(List(_)).pure[F]
 
 
-  def all[F[_] : Applicative]: Batching[F] = new Batching[F] {
-    def apply(aws: List[AtomicWrite]) = List(aws).pure[F]
-  }
+  def all[F[_] : Applicative]: Batching[F] = (aws: List[AtomicWrite]) => List(aws).pure[F]
 
 
-  def byNumberOfEvents[F[_] : Applicative](maxEventsInBatch: Int): Batching[F] = new Batching[F] {
-    def apply(aws: List[AtomicWrite]) = {
+  def byNumberOfEvents[F[_] : Applicative](maxEventsInBatch: Int): Batching[F] = {
+    aws: List[AtomicWrite] => {
       GroupByWeight[AtomicWrite](aws, maxEventsInBatch)(_.payload.size).pure[F]
     }
   }
--- persistence/src/main/scala/akka/persistence/kafka/journal/KafkaJournalConfig.scala
+++ persistence/src/main/scala/akka/persistence/kafka/journal/KafkaJournalConfig.scala
@@ -13,7 +13,7 @@ final case class KafkaJournalConfig(
   cassandra: EventualCassandraConfig = EventualCassandraConfig.default,
   startTimeout: FiniteDuration = 1.minute,
   stopTimeout: FiniteDuration = 1.minute,
-  maxEventsInBatch: Int = 10,
+  maxEventsInBatch: Int = 100,
   callTimeThresholds: CallTimeThresholds = CallTimeThresholds.default,
   jsonCodec: KafkaJournalConfig.JsonCodec = KafkaJournalConfig.JsonCodec.PlayJson)
 
