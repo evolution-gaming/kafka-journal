95f22605ea97a125b6b473bd0f261a2a09ddae89
Scala Steward <me@scala-steward.org>
2020-08-29 20:17:02 +0200

    Update scodec-bits to 1.1.19

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -61,7 +61,7 @@ object Dependencies {
 
   object Scodec {
     val core = "org.scodec" %% "scodec-core" % "1.11.7"
-    val bits = "org.scodec" %% "scodec-bits" % "1.1.14"
+    val bits = "org.scodec" %% "scodec-bits" % "1.1.19"
   }
 
   object Smetrics {
