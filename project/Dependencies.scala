670f4e97b14673211cccd3b51ad4a839b494f67e
Yaroslav Klymko <t3hnar@gmail.com>
2020-09-10 16:54:03 +0200

    update dependencies

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,7 +3,7 @@ import sbt._
 object Dependencies {
 
   val prometheus           = "io.prometheus"           % "simpleclient"          % "0.6.0"
-  val scalatest            = "org.scalatest"          %% "scalatest"             % "3.0.8"
+  val scalatest            = "org.scalatest"          %% "scalatest"             % "3.0.9"
   val `scala-java8-compat` = "org.scala-lang.modules" %% "scala-java8-compat"    % "0.9.1"
   val `kind-projector`     = "org.typelevel"           % "kind-projector"        % "0.11.0"
   val `cassandra-driver`   = "com.datastax.cassandra"  % "cassandra-driver-core" % "3.10.1"
@@ -18,12 +18,12 @@ object Dependencies {
   val hostname             = "com.evolutiongaming"    %% "hostname"              % "0.1.2"
   val scassandra           = "com.evolutiongaming"    %% "scassandra"            % "3.0.2"
   val `cassandra-sync`     = "com.evolutiongaming"    %% "cassandra-sync"        % "1.0.8"
-  val `cats-helper`        = "com.evolutiongaming"    %% "cats-helper"           % "2.1.0"
+  val `cats-helper`        = "com.evolutiongaming"    %% "cats-helper"           % "2.1.1"
   val random               = "com.evolutiongaming"    %% "random"                % "0.0.7"
   val retry                = "com.evolutiongaming"    %% "retry"                 % "2.0.0"
   val sstream              = "com.evolutiongaming"    %% "sstream"               % "0.2.1"
   val skafka               = "com.evolutiongaming"    %% "skafka"                % "11.0.0"
-  val scache               = "com.evolutiongaming"    %% "scache"                % "2.2.1"
+  val scache               = "com.evolutiongaming"    %% "scache"                % "2.3.1"
   val `akka-test-actor`    = "com.evolutiongaming"    %% "akka-test-actor"       % "0.0.2"
 
   object Cats {
@@ -61,7 +61,7 @@ object Dependencies {
 
   object Scodec {
     val core = "org.scodec" %% "scodec-core" % "1.11.7"
-    val bits = "org.scodec" %% "scodec-bits" % "1.1.14"
+    val bits = "org.scodec" %% "scodec-bits" % "1.1.20"
   }
 
   object Smetrics {
