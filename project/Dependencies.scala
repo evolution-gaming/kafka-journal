817b7595093fe61a760aa87ec0c10aeb9d494a11
Scala Steward <me@scala-steward.org>
2020-09-10 09:53:51 +0200

    Update play-json to 2.9.1

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -7,7 +7,7 @@ object Dependencies {
   val `scala-java8-compat` = "org.scala-lang.modules" %% "scala-java8-compat"    % "0.9.1"
   val `kind-projector`     = "org.typelevel"           % "kind-projector"        % "0.11.0"
   val `cassandra-driver`   = "com.datastax.cassandra"  % "cassandra-driver-core" % "3.10.1"
-  val `play-json`          = "com.typesafe.play"      %% "play-json"             % "2.7.4"
+  val `play-json`          = "com.typesafe.play"      %% "play-json"             % "2.9.1"
   val `play-json-jsoniter` = "com.evolutiongaming"    %% "play-json-jsoniter"    % "0.7.1"
   val `executor-tools`     = "com.evolutiongaming"    %% "executor-tools"        % "1.0.2"
   val `config-tools`       = "com.evolutiongaming"    %% "config-tools"          % "1.0.3"
