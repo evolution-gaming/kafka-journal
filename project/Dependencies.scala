257d2bdddd53a585022fa8f5a4a0c22a0d605ccb
Scala Steward <me@scala-steward.org>
2020-12-19 12:16:15 +0100

    Update cats-core to 2.3.1

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -27,7 +27,7 @@ object Dependencies {
   val `akka-test-actor`    = "com.evolutiongaming"    %% "akka-test-actor"       % "0.0.2"
 
   object Cats {
-    val core   = "org.typelevel" %% "cats-core"   % "2.3.0"
+    val core   = "org.typelevel" %% "cats-core"   % "2.3.1"
     val effect = "org.typelevel" %% "cats-effect" % "2.3.1"
   }
 
