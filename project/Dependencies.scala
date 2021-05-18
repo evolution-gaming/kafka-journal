721040b180e8903b1ec7ff9148baaa4188a530f1
Scala Steward <me@scala-steward.org>
2021-05-18 18:32:21 +0200

    Update scodec-core to 1.11.8

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -60,7 +60,7 @@ object Dependencies {
   }
 
   object Scodec {
-    val core = "org.scodec" %% "scodec-core" % "1.11.7"
+    val core = "org.scodec" %% "scodec-core" % "1.11.8"
     val bits = "org.scodec" %% "scodec-bits" % "1.1.20"
   }
 
