04abbb22df71ba72fba2b30727a0059c94704eca
Yaroslav Klymko <t3hnar@gmail.com>
2021-07-29 11:39:00 +0300

    update Version

--- journal/src/main/scala/com/evolutiongaming/kafka/journal/Version.scala
+++ journal/src/main/scala/com/evolutiongaming/kafka/journal/Version.scala
@@ -11,7 +11,7 @@ final case class Version(value: String) {
 
 object Version {
 
-  val current: Version = Version("0.0.156")
+  val current: Version = Version("0.0.158")
 
 
   implicit val eqVersion: Eq[Version] = Eq.fromUniversalEquals
