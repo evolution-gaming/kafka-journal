0a457d05ad69c8fe5f3ed37e7127d192629eeb68
Denys Fakhritdinov <dfakhritdinov@evolution.com>
2021-08-09 15:08:17 +0200

    bump cats-helper version w/ log MDC

--- journal/src/main/scala/com/evolutiongaming/kafka/journal/LogFromAkka.scala
+++ journal/src/main/scala/com/evolutiongaming/kafka/journal/LogFromAkka.scala
@@ -3,45 +3,57 @@ package com.evolutiongaming.kafka.journal
 import akka.event.LoggingAdapter
 import cats.effect.Sync
 import com.evolutiongaming.catshelper.Log
+import org.slf4j.MDC
 
 object LogFromAkka {
 
   def apply[F[_] : Sync](log: LoggingAdapter): Log[F] = new Log[F] {
 
-    def debug(msg: => String) = {
+    def debug(msg: => String, mdc: Log.Mdc) = {
       Sync[F].delay {
-        if (log.isDebugEnabled) log.debug(msg)
+        if (log.isDebugEnabled) withMDC(mdc) { log.debug(msg) }
       }
     }
 
-    def info(msg: => String) = {
+    def info(msg: => String, mdc: Log.Mdc) = {
       Sync[F].delay {
-        if (log.isInfoEnabled) log.info(msg)
+        if (log.isInfoEnabled) withMDC(mdc) { log.info(msg) }
       }
     }
 
-    def warn(msg: => String) = {
+    def warn(msg: => String, mdc: Log.Mdc) = {
       Sync[F].delay {
-        if (log.isWarningEnabled) log.warning(msg)
+        if (log.isWarningEnabled) withMDC(mdc) { log.warning(msg) }
       }
     }
 
-    def warn(msg: => String, cause: Throwable) = {
+    def warn(msg: => String, cause: Throwable, mdc: Log.Mdc) = {
       Sync[F].delay {
-        if (log.isWarningEnabled) log.warning(s"$msg: $cause")
+        if (log.isWarningEnabled) withMDC(mdc) { log.warning(s"$msg: $cause") }
       }
     }
 
-    def error(msg: => String) = {
+    def error(msg: => String, mdc: Log.Mdc) = {
       Sync[F].delay {
-        if (log.isErrorEnabled) log.error(msg)
+        if (log.isErrorEnabled) withMDC(mdc) { log.error(msg) }
       }
     }
 
-    def error(msg: => String, cause: Throwable) = {
+    def error(msg: => String, cause: Throwable, mdc: Log.Mdc) = {
       Sync[F].delay {
-        if (log.isErrorEnabled) log.error(cause, msg)
+        if (log.isErrorEnabled) withMDC(mdc) { log.error(cause, msg) }
       }
+    }
+
+    def withMDC(mdc: Log.Mdc)(log: => Unit): Unit =
+      mdc.context match {
+        case None => log
+        case Some(mdc) =>
+          val backup = MDC.getCopyOfContextMap
+          MDC.clear()
+          mdc.toSortedMap foreach { case (k, v) => MDC.put(k, v) }
+          log
+          MDC.setContextMap(backup)
     }
   }
 }
