790a34534123a8f256dede867594f6ab8b188565
Yaroslav Klymko <t3hnar@gmail.com>
2021-09-08 09:29:02 +0200

    fix LogFromAkka, ensure it does not out mdc on log arguments place

--- journal/src/main/scala/com/evolutiongaming/kafka/journal/LogFromAkka.scala
+++ journal/src/main/scala/com/evolutiongaming/kafka/journal/LogFromAkka.scala
@@ -6,41 +6,41 @@ import com.evolutiongaming.catshelper.Log
 
 object LogFromAkka {
 
-  def apply[F[_] : Sync](log: LoggingAdapter): Log[F] = new Log[F] {
+  def apply[F[_]: Sync](log: LoggingAdapter): Log[F] = new Log[F] {
 
     def debug(msg: => String, mdc: Log.Mdc) = {
       Sync[F].delay {
-        if (log.isDebugEnabled) log.debug(msg, mdc)
+        if (log.isDebugEnabled) log.debug(msg)
       }
     }
 
     def info(msg: => String, mdc: Log.Mdc) = {
       Sync[F].delay {
-        if (log.isInfoEnabled) log.info(msg, mdc)
+        if (log.isInfoEnabled) log.info(msg)
       }
     }
 
     def warn(msg: => String, mdc: Log.Mdc) = {
       Sync[F].delay {
-        if (log.isWarningEnabled) log.warning(msg, mdc)
+        if (log.isWarningEnabled) log.warning(msg)
       }
     }
 
     def warn(msg: => String, cause: Throwable, mdc: Log.Mdc) = {
       Sync[F].delay {
-        if (log.isWarningEnabled) log.warning(s"$msg: $cause", mdc)
+        if (log.isWarningEnabled) log.warning(s"$msg: $cause")
       }
     }
 
     def error(msg: => String, mdc: Log.Mdc) = {
       Sync[F].delay {
-        if (log.isErrorEnabled) log.error(msg, mdc)
+        if (log.isErrorEnabled) log.error(msg)
       }
     }
 
     def error(msg: => String, cause: Throwable, mdc: Log.Mdc) = {
       Sync[F].delay {
-        if (log.isErrorEnabled) log.error(cause, msg, mdc)
+        if (log.isErrorEnabled) log.error(cause, msg)
       }
     }
 
