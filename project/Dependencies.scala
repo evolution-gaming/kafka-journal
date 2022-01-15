434b0bf550ad5ee39db0ec09740a94c9e6e7ac59
Scala Steward <me@scala-steward.org>
2022-01-15 02:29:37 +0100

    Update skafka to 12.0.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -22,7 +22,7 @@ object Dependencies {
   val random               = "com.evolutiongaming"    %% "random"                % "0.1.1"
   val retry                = "com.evolutiongaming"    %% "retry"                 % "2.1.1"
   val sstream              = "com.evolutiongaming"    %% "sstream"               % "0.2.1"
-  val skafka               = "com.evolutiongaming"    %% "skafka"                % "11.8.0"
+  val skafka               = "com.evolutiongaming"    %% "skafka"                % "12.0.0"
   val scache               = "com.evolutiongaming"    %% "scache"                % "3.2.1"
   val `akka-test-actor`    = "com.evolutiongaming"    %% "akka-test-actor"       % "0.1.0"
 
