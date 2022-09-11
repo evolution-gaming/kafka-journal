f4c9bcebfbd5abf6c56729a12bfeb9fcbccac82a
Scala Steward <scala_steward@virtuslab.com>
2022-09-11 14:11:03 +0000

    Update scache to 4.0.3

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -22,7 +22,7 @@ object Dependencies {
   val retry                = "com.evolutiongaming"    %% "retry"                 % "3.0.1"
   val sstream              = "com.evolutiongaming"    %% "sstream"               % "1.0.1"
   val skafka               = "com.evolutiongaming"    %% "skafka"                % "14.0.0"
-  val scache               = "com.evolutiongaming"    %% "scache"                % "4.0.0"
+  val scache               = "com.evolutiongaming"    %% "scache"                % "4.0.3"
   val `akka-test-actor`    = "com.evolutiongaming"    %% "akka-test-actor"       % "0.1.0"
 
   object Cats {
