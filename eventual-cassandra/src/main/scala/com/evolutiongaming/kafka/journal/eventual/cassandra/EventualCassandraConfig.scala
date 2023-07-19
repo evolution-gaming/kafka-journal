2c132378e33592169f4668292dd928d9be6a9d73
Ruslans Tarasovs <ruslan@tarasovs.com>
2023-07-19 13:57:02 +0300

    More detailed scaladoc for EventualCassandraConfig

--- eventual-cassandra/src/main/scala/com/evolutiongaming/kafka/journal/eventual/cassandra/EventualCassandraConfig.scala
+++ eventual-cassandra/src/main/scala/com/evolutiongaming/kafka/journal/eventual/cassandra/EventualCassandraConfig.scala
@@ -11,15 +11,24 @@ import pureconfig.ConfigReader
   *
   * @param retries
   *   Number of retries in
-  *   [[com.evolutiongaming.scassandra.NextHostRetryPolicy]].
+  *   [[com.evolutiongaming.scassandra.NextHostRetryPolicy]]. It will retry
+  *   doing a request on the same host if it timed out, or switch to another
+  *   host if error happened, or the host was not available on a first attempt.
   * @param segmentSize
-  *   Number of records per one segment. See [[SegmentSize]] for more details.
+  *   Number of records per one segment. The larger the number, the better the
+  *   chance the recovery will hit the same partition, and be faster, but too
+  *   large numbers are also will make larger Cassandra partitions with all the
+  *   associated issues. See [[SegmentSize]] for more details.
   * @param client
   *   Cassandra client configuration, see [[CassandraConfig]] for more details.
   * @param schema
-  *   Schema of Cassandra database, i.e. keyspace, names of the tables etc.
+  *   Schema of Cassandra database, i.e. keyspace, names of the tables etc. It
+  *   also contains a flag if schema should be automatically created if not
+  *   present, which is useful for integration testing purposes etc.
   * @param consistencyConfig
-  *   Consistency levels to use for read and write statements to Cassandra.
+  *   Consistency levels to use for read and for write statements to Cassandra.
+  *   The main reason one may be interested to change it, is for integration
+  *   tests with small number of Cassandra nodes.
   */
 final case class EventualCassandraConfig(
   retries: Int = 100,
