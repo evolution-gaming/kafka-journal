5fcb2c82f568b375c6fcab40dcee9cb9821adab6
Scala Steward <scala_steward@virtuslab.com>
2023-08-05 14:47:55 +0000

    Update smetrics to 2.1.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -65,7 +65,7 @@ object Dependencies {
   }
 
   object Smetrics {
-    private val version = "2.0.0"
+    private val version = "2.1.0"
     val smetrics   = "com.evolutiongaming" %% "smetrics"            % version
     val prometheus = "com.evolutiongaming" %% "smetrics-prometheus" % version
   }
