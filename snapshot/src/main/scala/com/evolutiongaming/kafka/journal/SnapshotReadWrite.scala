e8434196bc42b286dfd008c1b842e2c32566cb2c
Ruslans Tarasovs <ruslan@tarasovs.com>
2023-11-16 13:44:59 +0200

    Add adapter factories.

--- snapshot/src/main/scala/com/evolutiongaming/kafka/journal/SnapshotReadWrite.scala
+++ snapshot/src/main/scala/com/evolutiongaming/kafka/journal/SnapshotReadWrite.scala
@@ -4,3 +4,12 @@ import com.evolutiongaming.kafka.journal.eventual.EventualRead
 import com.evolutiongaming.kafka.journal.eventual.EventualWrite
 
 final case class SnapshotReadWrite[F[_], A](eventualRead: EventualRead[F, A], eventualWrite: EventualWrite[F, A])
+object SnapshotReadWrite {
+
+  def of[F[_], A](implicit
+    eventualRead: EventualRead[F, A],
+    eventualWrite: EventualWrite[F, A]
+  ): SnapshotReadWrite[F, A] =
+    SnapshotReadWrite(eventualRead, eventualWrite)
+
+}
