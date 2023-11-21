edc45a13cc268b2195cd529875cf68ef39b836ff
Ruslans Tarasovs <ruslan@tarasovs.com>
2023-11-21 15:21:50 +0200

    Enable running Cassandra before test runs.

--- tests/src/test/scala/akka/persistence/kafka/journal/SnapshotSpec.scala
+++ tests/src/test/scala/akka/persistence/kafka/journal/SnapshotSpec.scala
@@ -3,7 +3,7 @@ package akka.persistence.kafka.journal
 import akka.persistence.snapshot.SnapshotStoreSpec
 import com.typesafe.config.ConfigFactory
 
-class SnapshotSpec extends SnapshotStoreSpec(ConfigFactory.load("integration.conf")) {
+class SnapshotSpec extends SnapshotStoreSpec(ConfigFactory.load("integration.conf")) with KafkaPluginSpec {
 
   override def supportsSerialization = false
 
