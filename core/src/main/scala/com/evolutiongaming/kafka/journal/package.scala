214a012cf09de012d85171220868a3353738315a
Ruslans Tarasovs <ruslan@tarasovs.com>
2023-12-18 11:42:39 +0200

    Remove duplicate class.

--- core/src/main/scala/com/evolutiongaming/kafka/journal/package.scala
+++ core/src/main/scala/com/evolutiongaming/kafka/journal/package.scala
@@ -1,9 +1,19 @@
 package com.evolutiongaming.kafka
 
+import com.evolutiongaming.skafka.consumer.{ConsumerRecord, ConsumerRecords}
+import scodec.bits.ByteVector
+
 package object journal {
 
   type Tag = String
 
   type Tags = Set[Tag]
 
+
+  type Headers = Map[String, String]
+
+
+  type ConsRecord = ConsumerRecord[String, ByteVector]
+
+  type ConsRecords = ConsumerRecords[String, ByteVector]
 }
