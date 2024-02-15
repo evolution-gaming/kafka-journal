67efbe08643841e3c90c4456e7312dae0da07b68
Ruslans Tarasovs <ruslan@tarasovs.com>
2024-02-15 11:33:37 +0200

    Start working on snapshot adapter.
    # Conflicts:
    #	snapshot/src/main/scala/com/evolutiongaming/kafka/journal/SnapshotSelectionCriteria.scala

--- snapshot/src/main/scala/com/evolutiongaming/kafka/journal/SnapshotStoreFlat.scala
+++ snapshot/src/main/scala/com/evolutiongaming/kafka/journal/SnapshotStoreFlat.scala
@@ -2,21 +2,13 @@ package com.evolutiongaming.kafka.journal
 
 import com.evolutiongaming.kafka.journal.eventual.EventualPayloadAndType
 
-import java.time.Instant
-
 trait SnapshotStoreFlat[F[_]] {
 
   def save(key: Key, snapshot: SnapshotRecord[EventualPayloadAndType]): F[Unit]
 
-  def load(
-    key: Key,
-    maxSeqNr: SeqNr,
-    maxTimestamp: Instant,
-    minSeqNr: SeqNr,
-    minTimestamp: Instant
-  ): F[Option[SnapshotRecord[EventualPayloadAndType]]]
+  def load(key: Key, criteria: SnapshotSelectionCriteria): F[Option[SnapshotRecord[EventualPayloadAndType]]]
 
-  def drop(key: Key, maxSeqNr: SeqNr, maxTimestamp: Instant, minSeqNr: SeqNr, minTimestamp: Instant): F[Unit]
+  def drop(key: Key, criteria: SnapshotSelectionCriteria): F[Unit]
 
   def drop(key: Key, seqNr: SeqNr): F[Unit]
 
