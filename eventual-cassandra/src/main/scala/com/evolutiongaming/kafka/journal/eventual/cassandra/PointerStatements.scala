6d80f7cce1ab6b6e323af797b624c6885371c178
Ruslans Tarasovs <ruslan@tarasovs.com>
2024-03-07 13:36:26 +0200

    Rollback changes to imports to minimize number of changes.

--- eventual-cassandra/src/main/scala/com/evolutiongaming/kafka/journal/eventual/cassandra/PointerStatements.scala
+++ eventual-cassandra/src/main/scala/com/evolutiongaming/kafka/journal/eventual/cassandra/PointerStatements.scala
@@ -6,8 +6,8 @@ import com.datastax.driver.core.GettableByNameData
 import com.evolutiongaming.catshelper.DataHelper._
 import com.evolutiongaming.kafka.journal.eventual.cassandra.CassandraHelper._
 import com.evolutiongaming.kafka.journal.util.SkafkaHelper._
-import com.evolutiongaming.scassandra.syntax._
 import com.evolutiongaming.scassandra.{DecodeRow, TableName}
+import com.evolutiongaming.scassandra.syntax._
 import com.evolutiongaming.skafka.{Offset, Partition, Topic}
 
 import java.time.Instant
