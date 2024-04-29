5c61b382336d9f11996124e35203a12b58054719
Mareks Rampāns <8796159+mr-git@users.noreply.github.com>
2024-04-29 22:53:25 +0300

    Revert "Update smetrics to 2.2.0 (#547)" (#615)
    This reverts commit e635fe8d99ead2f8c685fa064c0707567504672c.

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -64,7 +64,7 @@ object Dependencies {
   }
 
   object Smetrics {
-    private val version = "2.2.0"
+    private val version = "2.0.0"
     val smetrics   = "com.evolutiongaming" %% "smetrics"            % version
     val prometheus = "com.evolutiongaming" %% "smetrics-prometheus" % version
   }
