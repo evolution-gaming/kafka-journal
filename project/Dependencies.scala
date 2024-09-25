471ee7a421cc6696dc2381e30fba152e22a61809
Nikita Vilunov <nikita@vilunov.me>
2024-09-25 12:21:04 +0200

    Bump skafka

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -19,7 +19,7 @@ object Dependencies {
   val random                     = "com.evolutiongaming"    %% "random"                         % "1.0.0"
   val retry                      = "com.evolutiongaming"    %% "retry"                          % "3.0.1"
   val sstream                    = "com.evolutiongaming"    %% "sstream"                        % "1.0.1"
-  val skafka                     = "com.evolutiongaming"    %% "skafka"                         % "16.0.0"
+  val skafka                     = "com.evolutiongaming"    %% "skafka"                         % "17.1.0"
   val `akka-test-actor`          = "com.evolutiongaming"    %% "akka-test-actor"                % "0.1.0"
   val scache                     = "com.evolution"          %% "scache"                         % "5.0.0"
   val `resource-pool`            = "com.evolution"          %% "resource-pool"                  % "1.0.4"
