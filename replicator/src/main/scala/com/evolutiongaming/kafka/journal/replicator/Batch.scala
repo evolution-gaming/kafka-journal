8ff0ef8896f793ef8c7f77c80d79eb9a3f9ed1ce
Mareks Rampāns <8796159+mr-git@users.noreply.github.com>
2024-10-25 16:44:02 +0300

    attempt to improve the style - move `ActionRecord` handling inside `State`

--- replicator/src/main/scala/com/evolutiongaming/kafka/journal/replicator/Batch.scala
+++ replicator/src/main/scala/com/evolutiongaming/kafka/journal/replicator/Batch.scala
@@ -23,51 +23,7 @@ private[journal] sealed abstract class Batch extends Product {
 private[journal] object Batch {
 
   def of(records: NonEmptyList[ActionRecord[Action]]): List[Batch] = {
-
-    val state = records.reverse.foldLeft(State()) {
-      // ignore all actions before `Purge`
-      case (state, _) if state.purge.nonEmpty =>
-        state
-
-      case (state, ActionRecord(purge: Action.Purge, po)) =>
-        state.copy(
-          purge = Purge(po.offset, purge.origin, purge.version).some,
-        )
-
-      case (state, ActionRecord(delete: Action.Delete, po)) =>
-        val delete_ = state.delete match {
-          case Some(younger) =>
-            // take `origin` and `version` from "older" entity, if it has them
-            val origin  = delete.origin.orElse(younger.origin)
-            val version = delete.version.orElse(younger.version)
-            // make `Delete` action with largest `seqNr` and largest `offset`
-            if (younger.to < delete.to) Delete(po.offset, delete.to, origin, version)
-            else younger.copy(origin = origin, version = version)
-
-          case None =>
-            Delete(po.offset, delete.to, delete.origin, delete.version)
-        }
-        state.copy(
-          delete = delete_.some,
-        )
-
-      case (state, ActionRecord(append: Action.Append, po)) if state.delete.forall(_.to.value <= append.range.to) =>
-        val appends = state.appends match {
-          case Some(appends) =>
-            appends.copy(records = ActionRecord(append, po) :: appends.records)
-          case None =>
-            Appends(po.offset, NonEmptyList.of(ActionRecord(append, po)))
-        }
-        state.copy(
-          appends = appends.some,
-        )
-
-      case (state, _) =>
-        // ignore `Action.Append`, if it would get deleted and ignore `Action.Mark`
-        state
-    }
-
-    state.batches
+    records.reverse.foldLeft(State()) { _.handle(_) }.batches
   }
 
   private final case class State(
@@ -75,6 +31,64 @@ private[journal] object Batch {
     appends: Option[Appends] = None,
     delete: Option[Delete]   = None,
   ) {
+
+    def handle: ActionRecord[Action] => State = {
+      case _ if this.purge.nonEmpty => // ignore all actions before `Purge`
+        this
+
+      case ActionRecord(_: Action.Mark, _) =>
+        this
+
+      case ActionRecord(purge: Action.Purge, partitionOffset: PartitionOffset) =>
+        handlePurge(purge, partitionOffset)
+
+      case ActionRecord(delete: Action.Delete, partitionOffset: PartitionOffset) =>
+        handleDelete(delete, partitionOffset)
+
+      case ActionRecord(append: Action.Append, partitionOffset: PartitionOffset) =>
+        handleAppend(append, partitionOffset)
+    }
+
+    private def handlePurge(purge: Action.Purge, partitionOffset: PartitionOffset): State = {
+      this.copy(
+        purge = Purge(partitionOffset.offset, purge.origin, purge.version).some,
+      )
+    }
+
+    private def handleDelete(delete: Action.Delete, partitionOffset: PartitionOffset): State = {
+      val delete_ = this.delete match {
+        case Some(younger) =>
+          // take `origin` and `version` from "older" entity, if it has them
+          val origin  = delete.origin.orElse(younger.origin)
+          val version = delete.version.orElse(younger.version)
+          // make `Delete` action with largest `seqNr` and largest `offset`
+          if (younger.to < delete.to) Delete(partitionOffset.offset, delete.to, origin, version)
+          else younger.copy(origin = origin, version = version)
+
+        case None =>
+          Delete(partitionOffset.offset, delete.to, delete.origin, delete.version)
+      }
+      this.copy(
+        delete = delete_.some,
+      )
+    }
+
+    private def handleAppend(append: Action.Append, partitionOffset: PartitionOffset): State = {
+      // ignore `Action.Append`, if it would get deleted
+      if (this.delete.forall(_.to.value <= append.range.to)) {
+        val appends = this.appends match {
+          case Some(appends) =>
+            appends.copy(records = ActionRecord(append, partitionOffset) :: appends.records)
+          case None =>
+            Appends(partitionOffset.offset, NonEmptyList.of(ActionRecord(append, partitionOffset)))
+        }
+        this.copy(
+          appends = appends.some,
+        )
+      } else {
+        this
+      }
+    }
 
     def batches: List[Batch] = {
       // we can drop first `append`, if `deleteTo` will discard it AND there is at least one more `append`
