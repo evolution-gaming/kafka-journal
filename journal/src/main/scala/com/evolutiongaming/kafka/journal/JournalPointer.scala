3480b68b5a757aa3cde6dc93db948d6fa9d519ec
Denys Fakhritdinov <dfakhritdinov@evolution.com>
2024-11-14 13:56:00 +0100

    make JournalPointer public

--- journal/src/main/scala/com/evolutiongaming/kafka/journal/JournalPointer.scala
+++ journal/src/main/scala/com/evolutiongaming/kafka/journal/JournalPointer.scala
@@ -2,7 +2,7 @@ package com.evolutiongaming.kafka.journal
 
 import com.evolutiongaming.skafka.{Offset, Partition}
 
-private[journal] final case class JournalPointer(partitionOffset: PartitionOffset, seqNr: SeqNr) {
+final case class JournalPointer(partitionOffset: PartitionOffset, seqNr: SeqNr) {
 
   def offset: Offset = partitionOffset.offset
 
