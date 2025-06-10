21653919ee34a49a630ac9b8a01a5c4cdf83201b
Mareks Rampāns <8796159+mr-git@users.noreply.github.com>
2025-06-10 16:12:59 +0300

    discard unneeded parts in both forks

--- eventual-cassandra/src/main/scala/com/evolutiongaming/kafka/journal/cassandra/RecordMetadataExtension.scala
+++ eventual-cassandra/src/main/scala/com/evolutiongaming/kafka/journal/cassandra/RecordMetadataExtension.scala
@@ -1,34 +1,12 @@
-package com.evolutiongaming.kafka.journal
+package com.evolutiongaming.kafka.journal.cassandra
 
-import com.evolutiongaming.kafka.journal.util.PlayJsonHelper.*
+import com.evolutiongaming.kafka.journal.cassandra.PlayJsonHelperExtension.*
+import com.evolutiongaming.kafka.journal.{JsonCodec, RecordMetadata}
 import com.evolutiongaming.scassandra.{DecodeByName, DecodeRow, EncodeByName, EncodeRow}
-import play.api.libs.json.*
-import play.api.libs.json.Json.WithDefaultValues
 
 import scala.util.Try
 
-final case class RecordMetadata(
-  header: HeaderMetadata = HeaderMetadata.empty,
-  payload: PayloadMetadata = PayloadMetadata.empty,
-)
-
-object RecordMetadata {
-
-  val empty: RecordMetadata = RecordMetadata()
-
-  implicit val formatRecordMetadata: OFormat[RecordMetadata] = {
-    // adding WithDefaultValues, so on Scala 2 and Scala 3 the format behaves the same:
-    // https://github.com/playframework/play-json/issues/1146
-    val format = Json.using[WithDefaultValues].format[RecordMetadata]
-    val reads = format
-      .filter(_ != empty) // if empty is parsed, we might be dealing with an older format - HeaderMetadata
-      .orElse { (json: JsValue) =>
-        json
-          .validate[HeaderMetadata]
-          .map { a => RecordMetadata(a, PayloadMetadata.empty) }
-      }
-    OFormat(reads, format)
-  }
+object RecordMetadataExtension {
 
   implicit def encodeByNameRecordMetadata(
     implicit
@@ -53,11 +31,4 @@ object RecordMetadata {
     decode: JsonCodec.Decode[Try],
   ): DecodeRow[RecordMetadata] =
     DecodeRow("metadata")
-
-  implicit class RecordMetadataOps(val self: RecordMetadata) extends AnyVal {
-
-    def withExpireAfter(expireAfter: Option[ExpireAfter]): RecordMetadata = {
-      self.copy(payload = self.payload.copy(expireAfter = expireAfter))
-    }
-  }
 }
