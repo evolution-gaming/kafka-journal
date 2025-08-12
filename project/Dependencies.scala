c2dea045d4538f0bd0eb28889d1793a9c6c9e294
Mareks Rampāns <8796159+mr-git@users.noreply.github.com>
2025-08-12 12:30:15 +0300

    update pekko-extension to 1.0.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -8,7 +8,7 @@ object Dependencies {
   val KafkaClients = "org.apache.kafka" % "kafka-clients" % "3.4.0"
   val PlayJson = "com.typesafe.play" %% "play-json" % "2.10.7"
 
-  private val PekkoExtensionVersion = "0.0.6"
+  private val PekkoExtensionVersion = "1.0.0"
   val PlayJsonJsoniter = "com.evolution" %% "play-json-jsoniter" % "1.2.3"
   val ExecutorTools = "com.evolutiongaming" %% "executor-tools" % "1.0.5"
   val AkkaSerialization = "com.evolutiongaming" %% "akka-serialization" % "1.1.0"
