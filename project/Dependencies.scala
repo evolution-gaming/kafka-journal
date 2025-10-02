d92ed10f3c58a2fee84b7304a56dbe20f6825f48
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2025-10-02 16:30:35 +0200

    Update logback-classic, logback-core to 1.5.19 (#809)
    ## About this PR
    📦 Updates 
    * [ch.qos.logback:logback-classic](https://github.com/qos-ch/logback)
    * [ch.qos.logback:logback-core](https://github.com/qos-ch/logback)
    
     from `1.5.18` to `1.5.19`
    
    ## Usage
    ✅ **Please merge!**
    
    I'll automatically update this PR to resolve conflicts as long as you
    don't change it yourself.
    
    If you'd like to skip this version, you can just close this PR. If you
    have any feedback, just mention me in the comments below.
    
    Configure Scala Steward for your repository with a
    [`.scala-steward.conf`](https://github.com/scala-steward-org/scala-steward/blob/7e9cf7fdcaebf97011b06af08f09f37d1415544b/docs/repo-specific-configuration.md)
    file.
    
    _Have a fantastic day writing Scala!_
    
    <details>
    <summary>⚙ Adjust future updates</summary>
    
    Add this to your `.scala-steward.conf` file to ignore future updates of
    this dependency:
    ```
    updates.ignore = [ { groupId = "ch.qos.logback" } ]
    ```
    Or, add this to slow down future updates of this dependency:
    ```
    dependencyOverrides = [{
      pullRequests = { frequency = "30 days" },
      dependency = { groupId = "ch.qos.logback" }
    }]
    ```
    </details>
    
    <sup>
    labels: test-library-update, early-semver-patch, semver-spec-patch,
    commit-count:1
    </sup>
    
    <!-- scala-steward = {
      "Update" : {
        "ForGroupId" : {
          "forArtifactIds" : [
            {
              "ForArtifactId" : {
                "crossDependency" : [
                  {
                    "groupId" : "ch.qos.logback",
                    "artifactId" : {
                      "name" : "logback-classic",
                      "maybeCrossName" : null
                    },
                    "version" : "1.5.18",
                    "sbtVersion" : null,
                    "scalaVersion" : null,
                    "configurations" : "test"
                  }
                ],
                "newerVersions" : [
                  "1.5.19"
                ],
                "newerGroupId" : null,
                "newerArtifactId" : null
              }
            },
            {
              "ForArtifactId" : {
                "crossDependency" : [
                  {
                    "groupId" : "ch.qos.logback",
                    "artifactId" : {
                      "name" : "logback-core",
                      "maybeCrossName" : null
                    },
                    "version" : "1.5.18",
                    "sbtVersion" : null,
                    "scalaVersion" : null,
                    "configurations" : "test"
                  }
                ],
                "newerVersions" : [
                  "1.5.19"
                ],
                "newerGroupId" : null,
                "newerArtifactId" : null
              }
            }
          ]
        }
      },
      "Labels" : [
        "test-library-update",
        "early-semver-patch",
        "semver-spec-patch",
        "commit-count:1"
      ]
    } -->

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -32,7 +32,7 @@ object Dependencies {
   }
 
   object Logback {
-    private val version = "1.5.18"
+    private val version = "1.5.19"
     val Core = "ch.qos.logback" % "logback-core" % version
     val Classic = "ch.qos.logback" % "logback-classic" % version
   }
