2c77a96ced5e0398cdcee4a75936ae01c7c0fcb4
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2026-03-16 13:58:06 +0100

    Update cats-effect to 3.7.0 (#868)
    ## About this PR
    📦 Updates
    [org.typelevel:cats-effect](https://github.com/typelevel/cats-effect)
    from `3.6.3` to `3.7.0`
    
    📜 [GitHub Release
    Notes](https://github.com/typelevel/cats-effect/releases/tag/v3.7.0) -
    [Version
    Diff](https://github.com/typelevel/cats-effect/compare/v3.6.3...v3.7.0)
    
    ## Usage
    ✅ **Please merge!**
    
    I'll automatically update this PR to resolve conflicts as long as you
    don't change it yourself.
    
    If you'd like to skip this version, you can just close this PR. If you
    have any feedback, just mention me in the comments below.
    
    Configure Scala Steward for your repository with a
    [`.scala-steward.conf`](https://github.com/scala-steward-org/scala-steward/blob/fac8db33a2625ae29d1b95ab39a6807a1885deab/docs/repo-specific-configuration.md)
    file.
    
    _Have a fantastic day writing Scala!_
    
    <details>
    <summary>⚙ Adjust future updates</summary>
    
    Add this to your `.scala-steward.conf` file to ignore future updates of
    this dependency:
    ```
    updates.ignore = [ { groupId = "org.typelevel", artifactId = "cats-effect" } ]
    ```
    Or, add this to slow down future updates of this dependency:
    ```
    dependencyOverrides = [{
      pullRequests = { frequency = "30 days" },
      dependency = { groupId = "org.typelevel", artifactId = "cats-effect" }
    }]
    ```
    </details>
    
    <sup>
    labels: library-update, early-semver-minor, semver-spec-minor,
    version-scheme:early-semver, commit-count:1
    </sup>
    
    <!-- scala-steward = {
      "Update" : {
        "ForArtifactId" : {
          "crossDependency" : [
            {
              "groupId" : "org.typelevel",
              "artifactId" : {
                "name" : "cats-effect",
                "maybeCrossName" : "cats-effect_2.13"
              },
              "version" : "3.6.3",
              "sbtVersion" : null,
              "scalaVersion" : null,
              "configurations" : null
            },
            {
              "groupId" : "org.typelevel",
              "artifactId" : {
                "name" : "cats-effect",
                "maybeCrossName" : "cats-effect_3"
              },
              "version" : "3.6.3",
              "sbtVersion" : null,
              "scalaVersion" : null,
              "configurations" : null
            }
          ],
          "newerVersions" : [
            "3.7.0"
          ],
          "newerGroupId" : null,
          "newerArtifactId" : null
        }
      },
      "Labels" : [
        "library-update",
        "early-semver-minor",
        "semver-spec-minor",
        "version-scheme:early-semver",
        "commit-count:1"
      ]
    } -->

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -28,7 +28,7 @@ object Dependencies {
 
   object Cats {
     val Core = "org.typelevel" %% "cats-core" % "2.13.0"
-    val Effect = "org.typelevel" %% "cats-effect" % "3.6.3"
+    val Effect = "org.typelevel" %% "cats-effect" % "3.7.0"
   }
 
   object Logback {
