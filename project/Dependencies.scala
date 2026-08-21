b7b5248a6aae9c6cebb4cd4db6b6650a01a11d5c
Scala Steward <scala_steward@virtuslab.com>
2026-08-21 15:50:58 +0000

    Update pekko-persistence, ... to 1.7.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -53,7 +53,7 @@ object Dependencies {
   }
 
   object Pekko {
-    private val version = "1.6.0"
+    private val version = "1.7.0"
     val Actor = "org.apache.pekko" %% "pekko-actor" % version
     val Testkit = "org.apache.pekko" %% "pekko-testkit" % version
     val Stream = "org.apache.pekko" %% "pekko-stream" % version
