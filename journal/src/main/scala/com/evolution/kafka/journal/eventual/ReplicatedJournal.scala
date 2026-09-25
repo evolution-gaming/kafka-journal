1a2aef5a6169f5e25ce6c3b770b9cae9f4b32d0b
Ruslans Tarasovs <ruslan@tarasovs.com>
2026-09-25 15:57:52 +0300

    Improved wording of fork definition

--- journal/src/main/scala/com/evolution/kafka/journal/eventual/ReplicatedJournal.scala
+++ journal/src/main/scala/com/evolution/kafka/journal/eventual/ReplicatedJournal.scala
@@ -180,7 +180,7 @@ object ReplicatedJournal {
     def setSchemaVersion(version: Int): F[Unit]
 
     /**
-     * Called for every detected journal fork, i.e. an event whose `seqNr` did not increase.
+     * Called for every detected journal fork: an event with a repeated or out-of-order `seqNr`.
      *
      * @param duplicateProven
      *   true if the `seqNr` is known to be used already, false if the `seqNr` only went down
@@ -256,7 +256,7 @@ object ReplicatedJournal {
 
       val forkDetectedCounter = registry.counter(
         name = s"${ prefix }_fork_detected_total",
-        help = "Number of journal forks detected, i.e. events whose seqNr did not increase",
+        help = "Number of journal forks detected, i.e. events with a repeated or out-of-order seqNr",
         labels = LabelNames("topic", "kind"),
       )
 
